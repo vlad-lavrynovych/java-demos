7b9ebb0e6f6a2f7a4c9a85a25cbea8b7c4048a20
VLADORT <vlad30082001@gmail.com>
2022-02-15 22:55:15 +0200

    Added eureka discovery

--- rabbit-mq/src/main/java/com/javademos/rabbit_mq/RabbitMqApplication.java
+++ rabbit-mq/src/main/java/com/javademos/rabbit_mq/RabbitMqApplication.java
@@ -3,7 +3,7 @@ package com.javademos.rabbit_mq;
 import com.javademos.rabbit_mq.dto.Order;
 import com.javademos.rabbit_mq.service.RabbitMessagingService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -17,7 +17,7 @@ public class RabbitMqApplication {
     }
 
     @Bean
-    public ApplicationRunner runner(RabbitMessagingService messagingService) {
+    public CommandLineRunner runner(RabbitMessagingService messagingService) {
         log.info("\n====== Sending message...");
         return args -> messagingService.sendOrder(new Order(1, "TEST_ORDER"));
     }
