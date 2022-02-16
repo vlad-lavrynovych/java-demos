3e16f73ae25e9763c6e518ea04860cdb70589a05
vlad-lavrynovych <vlad300820001@gmail.com>
2022-02-16 09:27:33 +0200

    Added eureka load balancing

--- reactor/src/main/java/com/java_demos/reactor/ReactorApplication.java
+++ reactor/src/main/java/com/java_demos/reactor/ReactorApplication.java
@@ -3,10 +3,16 @@ package com.java_demos.reactor;
 import com.java_demos.reactor.domain.Ingredient;
 import io.r2dbc.spi.ConnectionFactory;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
@@ -16,7 +22,11 @@ import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 @Slf4j
+@EnableEurekaClient
 public class ReactorApplication {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     public static void main(String[] args) {
         SpringApplication.run(ReactorApplication.class, args);
@@ -33,12 +43,37 @@ public class ReactorApplication {
     }
 
     @Bean
+    @LoadBalanced
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+
+    @Bean
     public CommandLineRunner demo() {
 
         return (args) -> {
             Flux<Ingredient> ingredients = WebClient.create()
                     .get()
                     .uri("http://localhost:8083/ingredients")
+                    .retrieve()
+                    .bodyToFlux(Ingredient.class);
+            log.error("Extracted");
+            ingredients.subscribe(s -> log.error(s.getName()));
+        };
+    }
+
+
+    @Bean
+    public CommandLineRunner demo(@LoadBalanced WebClient.Builder webClientBuilder) {
+
+        discoveryClient.getInstances("reactor").forEach((ServiceInstance s) -> {
+            System.out.println(ToStringBuilder.reflectionToString(s));
+        });
+
+        return (args) -> {
+            Flux<Ingredient> ingredients = webClientBuilder.build()
+                    .get()
+                    .uri("http://reactor/ingredients")
                     .retrieve()
                     .bodyToFlux(Ingredient.class);
             log.error("Extracted");
